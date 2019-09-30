1d80d78a0b9f082994940f10731d9ee30a38261f
Volodymyr Medvid <vmedvid@riseup.net>
2019-09-30 23:36:43 +0300

    Drop appveyor config

--- ci/build.cmd
+++
@@ -1,7 +0,0 @@
-setlocal
-cd "%~dp0"
-
-@if defined BUILD_TYPE set BUILD_CMD=/p:Configuration=%BUILD_TYPE%
-@if defined TARGET set TARGET_CMD=/target:%TARGET%
-
-msbuild.cmd ..\foo_listenbrainz.sln %BUILD_CMD% %TARGET_CMD%
